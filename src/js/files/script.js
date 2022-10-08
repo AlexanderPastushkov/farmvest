151acfd3093e1d1d6abd3807350952a9e946d34a
Alexander <alexanderpastfast@gmail.com>
2022-10-08 14:27:09 +0300

    Godbless

---
+++ src/js/files/script.js
@@ -0,0 +1,13 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+
+// let lock ='Ваня, Коля, Оля';
+// let arr = lock.split('+');
+// console.log(typeof(loc));
+// console.log(typeof(arr));
+// console.log(arr);
+
+
